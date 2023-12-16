c0b3a1a299723b1632439c3a89a7684302af8629
Ryan Pek <ryanpekyize2001@gmail.com>
2023-12-16 12:47:49 +0800

    wrote a access validation foreach vertex, by default each room will have a "basic" clearance, routes will have basic clearance to traverse. To set the set of clearences for dd_vertex as basic or a specific clearance type. User will have a set called clearance card, to add the specified clearance to be able to traverse the vertex with specific access needed when coding the path finding to take note of the vertex that cannot be visited
    added vertex prefix function for graph creation
    to add new prefix as needed
    
    added user access card in user object, to use to find availiable routes in graph by eliminating inaccessible areas

--- SUTD_Map_project/SUTD_Map_project.py
+++ SUTD_Map_project/SUTD_Map_project.py
@@ -5,11 +5,16 @@ import User
 
 def testbench():
     gg = Graph.Graph()
-    print(gg.__dict__)
-    pass
+    vt_pf = gg.query_vertex_Prefix()
+    print(vt_pf)
+    uu = User.User()
+    uu.add_clearance()
+    print(uu.__dict__)
 
 def main():
     testbench()
+    
+
 
 if __name__ == '__main__':
     start_time = time.process_time()
--- SUTD_Map_project/User.py
+++ SUTD_Map_project/User.py
@@ -1,10 +1,38 @@
 
+
+selection_yes=['1','y','Y','Yes','yes']
+selection_no=['0','n','N','No','no']
+
 class User():
     def __init__(self):
-        self.clearance_card=[]
-        
-    def add_clearance_fablab(self):
-        self.clearance_card.append("FABLAB_ACCESS")
+        self.clearance_card={"Basic"}
         
     def add_clearance(self):
+        clearance_list=["Fablab_basic","Fablab_Woodwork","Fablab_Metalwork","Hostel_55","Hostel_57","Hostel_59"]
+        for i in range(len(clearance_list)):
+            print("{:02d}\t{}".format(i, clearance_list[i]))
+        while True:
+            try:
+                selection=int(input("\nPick a Clearance: "))
+                if selection>-1 and selection<len(clearance_list):
+                    self.clearance_card.add(clearance_list[selection])
+                    break
+                else:
+                    print("out of index")
+            except ValueError:
+                print("Not a valid input")
+                
+        print("\nConfirm selection \n{:02d}\t{}?\n".format(selection, clearance_list[selection]))
+        sel=input("y/n: ")
+        if sel in selection_yes:
+            print("Selected {}\n".format(clearance_list[selection]))
+            return clearance_list[selection]
+        elif sel in selection_no:
+            return self.query_vertex_Prefix()
+        else:
+            print("Not a valid input\n")
+            return self.query_vertex_Prefix()
+        
+        
+    def remove_clearance(self):
         pass
