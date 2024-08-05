5d1a329cbd88f8088182298920edb8eee1eb1136
peky <ryan_pek@mymail.sutd.edu.sg>
2024-08-06 02:56:40 +0800

    latest changes

--- SUTD_Map_project/Path_query.py
+++ SUTD_Map_project/Path_query.py
@@ -3,6 +3,14 @@ class Query():
         self.dd_locationid = idlkup
         print('test query class')
         print(self.dd_locationid)
+        self.welcome_message()
+        self.tempwaitinput()
 
     def __del__(self):
         print('test del query class')
+
+    def welcome_message(self):
+        print("placeholder message hello owo\n")
+
+    def tempwaitinput(self):
+        temp = input("Enter any key to continue:")
